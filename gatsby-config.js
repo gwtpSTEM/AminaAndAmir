8dd1df0c651a069acbff2dcb6c797468b67308c3
Arisa Williams <arisa@gwtp.us>
2020-07-15 00:40:58 -0400

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,19 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "The Awesome Adventures of Amina and Amir",
+        short_name: "The Awesome Adventures of Amina and Amir",
+        start_url: "/",
+        background_color: "#424284",
+        theme_color: "#424284",
+        display: "standalone",
+        icon: "src/images/icon.png",
+        crossOrigin: `use-credentials`,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
